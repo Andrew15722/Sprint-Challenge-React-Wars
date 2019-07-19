7c98d3ac2ace09add483dd6a9bb64293f146d534
andrew-mendez <andrew15722@yahoo.com>
2019-07-19 15:48:40 -0700

    met mvp goals, finished styling page and double checking for errors

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Divider } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 
 function StarWarsCard({ name, height, mass, hairColor, skinColor, eyeColor, birthYear, gender }) {
 	return (
-		<Card.Group itemsPerRow={4}>
-			<Card>
-				<ul>
-					<li className="card-li">{name}</li>
-					<li className="card-li">{height}</li>
-					<li className="card-li">{mass}</li>
-					<li className="card-li">{hairColor}</li>
-					<li className="card-li">{skinColor}</li>
-					<li className="card-li">{eyeColor}</li>
-					<li className="card-li">{birthYear}</li>
-					<li className="card-li">{gender}</li>
-				</ul>
-			</Card>
-		</Card.Group>
+		<Card>
+			<ul>
+				<li className="card-li">{name}</li>
+				<li className="card-li">{height}</li>
+				<li className="card-li">{mass}</li>
+				<li className="card-li">{hairColor}</li>
+				<li className="card-li">{skinColor}</li>
+				<li className="card-li">{eyeColor}</li>
+				<li className="card-li">{birthYear}</li>
+				<li className="card-li">{gender}</li>
+			</ul>
+		</Card>
 	);
 }
 
--- starwars/src/components/CardGrid.js
+++ starwars/src/components/CardGrid.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import StarWarsCard from './Card';
+import { Card } from 'semantic-ui-react';
 
 function CardGrid() {
 	//keep naming conventions simple.
@@ -15,19 +16,21 @@ function CardGrid() {
 		// don't forget to pay attention to divs and the overall formatting of your return.
 		// always add curly braces before you write javascript in JSX
 		<div>
-			{data.map((char) => (
-				<StarWarsCard
-					key={char.name}
-					name={char.name}
-					height={char.height}
-					mass={char.mass}
-					hairColor={char.hair_color}
-					skinColor={char.skin_color}
-					eyeColor={char.eye_color}
-					birthYear={char.birth_year}
-					gender={char.gender}
-				/>
-			))}
+			<Card.Group itemsPerRow={3}>
+				{data.map((char) => (
+					<StarWarsCard
+						key={char.name}
+						name={`Name: ${char.name}`}
+						height={`height: ${char.height}`}
+						mass={`mass: ${char.mass}`}
+						hairColor={`Hair Color:${char.hair_color}`}
+						skinColor={`Skin Color: ${char.skin_color}`}
+						eyeColor={`Eye Color: ${char.eye_color}`}
+						birthYear={`BirthYear: ${char.birth_year}`}
+						gender={`gender ${char.gender}`}
+					/>
+				))}
+			</Card.Group>
 		</div>
 	);
 }
